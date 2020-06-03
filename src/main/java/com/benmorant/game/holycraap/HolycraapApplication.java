1bf9716f2d99a4764687f764da03183bf674efc6
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-03 22:19:49 +0200

    completed a bit application.properties

--- src/main/java/com/benmorant/game/holycraap/HolycraapApplication.java
+++ src/main/java/com/benmorant/game/holycraap/HolycraapApplication.java
@@ -1,13 +1,33 @@
 package com.benmorant.game.holycraap;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HolycraapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolycraapApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Regardons ce que nous avons avec Spring Boot");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
