0bd2842f9f7d030b795bffe236335a086b3fac8d
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-03 22:04:01 +0200

    added

---
+++ src/main/java/com/benmorant/game/holycraap/HolycraapApplication.java
@@ -0,0 +1,13 @@
+package com.benmorant.game.holycraap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolycraapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolycraapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benmorant/game/holycraap/HolycraapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benmorant.game.holycraap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolycraapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
