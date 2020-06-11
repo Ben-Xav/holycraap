71d3f3ad6b33ee8a9f502cc0508de27a8c567ac4
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-11 10:06:56 +0200

    added class People with some fields and annotations.

---
+++ src/main/java/com/benmorant/game/holycraap/model/entity/People.java
@@ -0,0 +1,32 @@
+package com.benmorant.game.holycraap.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "people")
+public class People implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    private int currentHp;
+    private int hpMax;
+    private int currentMp;
+    private int mpMax;
+
+}
