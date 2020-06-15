8a8511cfe5e9d37e1be7f4ae1ce2ba1345284ab6
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-15 09:36:12 +0200

    added some values in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 /* People */
-INSERT INTO `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Loup Ardent', '12', '12', '6', '6');
+insert into `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Loup Ardent', '12', '12', '6', '6');
+insert into `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Hardy Xav', '1000', '1000', '1000', '1000');
+insert into `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Conan la Barbare', '14', '18', '4', '4');
