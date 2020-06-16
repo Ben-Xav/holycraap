c9a6d326bdcf96c564b89203ecbfbbc69fa1de9c
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-16 13:17:45 +0200

    formatted all my code with google checkstyle setting xml, available here : https://raw.githubusercontent.com/google/styleguide/gh-pages/intellij-java-google-style.xml.

--- src/main/java/com/benmorant/game/holycraap/model/business/PeopleService.java
+++ src/main/java/com/benmorant/game/holycraap/model/business/PeopleService.java
@@ -1,12 +1,11 @@
 package com.benmorant.game.holycraap.model.business;
 
 import com.benmorant.game.holycraap.model.entity.People;
-
 import java.util.List;
 
 public interface PeopleService {
 
-    People saveInBase(People people);
+  People saveInBase(People people);
 
     People getByName(String name);
 
