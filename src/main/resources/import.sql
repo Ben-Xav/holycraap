f3489a5695a9a7960e86e35a1776c7808de4ed9d
bmorant <b.morant@lefebvre-sarrut.eu>
2020-06-17 18:36:58 +0200

    completed import.sql
    fixed “Error executing DDL ”alter table xxx drop foreign key yyy“ via JDBC Statement” by modifying spring.jpa.hibernate.ddl-auto , from create-drop to update .

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 /* Item */
 insert into `item` (name, price, weight) values ('Dent de dragon', '100', '1');
+insert into `item` (name, price, weight) values ('salicorne', '2', '1');
+insert into `item` (name, price, weight) values ('gousse de mandragore', '5', '1');
 
 /* People */
 insert into `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Loup Ardent', '12', '12', '6', '6');
@@ -8,3 +10,5 @@ insert into `people` (name, current_hp, hp_max, current_mp, mp_max) values ('Con
 
 /* People-Item */
 insert into `people_item` (item_id, people_id) values ('1', '1');
+insert into `people_item` (item_id, people_id) values ('2', '3');
+insert into `people_item` (item_id, people_id) values ('3', '1');
