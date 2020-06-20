bd781ce026f284b9a1552f7cd255dc7d4df36659
Xavier Hardy <xavierhardy@users.noreply.github.com>
2020-06-20 10:05:22 +0100

    #12 fixed SpotBugs Gradle plugin and warning

--- src/main/java/com/benmorant/game/holycraap/HolycraapApplication.java
+++ src/main/java/com/benmorant/game/holycraap/HolycraapApplication.java
@@ -1,5 +1,6 @@
 package com.benmorant.game.holycraap;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -8,6 +9,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class HolycraapApplication extends SpringBootServletInitializer {
 
+  @SuppressFBWarnings("DLS_DEAD_LOCAL_STORE")
   public static void main(String[] args) {
 
       SpringApplication app = new SpringApplication(HolycraapApplication.class);
