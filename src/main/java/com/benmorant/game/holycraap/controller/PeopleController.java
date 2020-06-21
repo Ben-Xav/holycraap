37fefd45f3d914f2577db3b6620c746fea51f260
Ben Morant <52816705+benmorant@users.noreply.github.com>
2020-06-21 23:27:06 +0200

    modified PeopleController and add some methods in PeopleService and PeopleServiceImpl accordingly .

--- src/main/java/com/benmorant/game/holycraap/controller/PeopleController.java
+++ src/main/java/com/benmorant/game/holycraap/controller/PeopleController.java
@@ -2,41 +2,68 @@ package com.benmorant.game.holycraap.controller;
 
 import com.benmorant.game.holycraap.model.business.PeopleService;
 import com.benmorant.game.holycraap.model.entity.People;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/people")
+@RestController
+@RequestMapping("/api")
 public class PeopleController {
+
+  private final Logger log = LoggerFactory.getLogger(PeopleController.class);
+
   @Autowired private PeopleService peopleService;
 
-  /** This class just served as an attempt to follow one of Spring.io guides. */
-  @PostMapping(path = "/add")
-  public @ResponseBody String addNewPeople(
-      @RequestParam String name,
-      @RequestParam int currentHp,
-      @RequestParam int hpMax,
-      @RequestParam int currentMp,
-      @RequestParam int mpMax) {
-
-    People p = new People();
-    p.setName(name);
-    p.setCurrentHp(currentHp);
-    p.setHpMax(hpMax);
-    p.setCurrentMp(currentMp);
-    p.setMpMax(mpMax);
-    peopleService.saveInBase(p);
-    return name + "added to database !";
+  public PeopleController(PeopleService peopleService) {
+    this.peopleService = peopleService;
   }
 
-  @GetMapping(path = "/all")
-  public @ResponseBody Iterable<People> getAllPeople() {
-
+  @GetMapping("/people")
+  Collection<People> peopleCollection() {
     return peopleService.getAllPeople();
+  }
+
+  @GetMapping("/people/{id}")
+  ResponseEntity<?> getPeople(@PathVariable Integer id) {
+    Optional<People> peopleOptional = peopleService.getById(id);
+    return peopleOptional
+        .map(response -> ResponseEntity.ok().body(response))
+        .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+  }
+
+  @PostMapping("/people")
+  ResponseEntity<People> createPeople(@Valid @RequestBody People people) throws URISyntaxException {
+    log.info("Request to create group: {}", people);
+    People result = peopleService.saveInBase(people);
+    return ResponseEntity.created(new URI("/api/people/" + result.getId())).body(result);
+  }
+
+  @PutMapping("/people/{id}")
+  ResponseEntity<People> updatePeople(@Valid @RequestBody People people) {
+    log.info("Request to update people: {}", people);
+    People result = peopleService.saveInBase(people);
+    return ResponseEntity.ok().body(result);
+  }
+
+  @DeleteMapping("/people/{id}")
+  public ResponseEntity<?> deletePeople(@PathVariable Integer id) {
+    log.info("Request to delete people: {}", id);
+    peopleService.removeById(id);
+    return ResponseEntity.ok().build();
   }
 }
