324bcb7cf333942b9d14f151ec56b55058eac77a
Ben Morant <52816705+benmorant@users.noreply.github.com>
2020-07-03 13:25:50 +0200

    build OK, tests passed with lombok away
    cf https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/ among others

--- src/main/java/com/benmorant/game/holycraap/model/entity/Item.java
+++ src/main/java/com/benmorant/game/holycraap/model/entity/Item.java
@@ -6,11 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+// @Data
+// @NoArgsConstructor
 @Entity
 @Table(name = "item")
 public class Item implements Serializable {
@@ -18,11 +16,78 @@ public class Item implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
   private String name;
   private int slot;
 
   private double price;
+
+  public Item() {}
+
+  public Item(String name, int slot, double price) {
+    this.name = name;
+    this.slot = slot;
+    this.price = price;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getSlot() {
+    return slot;
+  }
+
+  public void setSlot(int slot) {
+    this.slot = slot;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Item)) return false;
+    return id != null && id.equals(((Item) o).getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return 31;
+  }
+
+  @Override
+  public String toString() {
+    return "Item{"
+        + "id="
+        + id
+        + ", name='"
+        + name
+        + '\''
+        + ", slot="
+        + slot
+        + ", price="
+        + price
+        + '}';
+  }
 }
--- src/main/java/com/benmorant/game/holycraap/model/entity/People.java
+++ src/main/java/com/benmorant/game/holycraap/model/entity/People.java
@@ -10,14 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@RequiredArgsConstructor
 @Entity
 @Table(name = "people")
 public class People implements Serializable {
@@ -25,7 +19,7 @@ public class People implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
   @NonNull private String name;
@@ -37,4 +31,110 @@ public class People implements Serializable {
   @OneToMany(fetch = FetchType.EAGER)
   @JoinColumn(name = "people_id")
   private List<Item> inventory;
+
+  public People() {}
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public People(
+      @NonNull String name,
+      int currentHp,
+      int hpMax,
+      int currentMp,
+      int mpMax,
+      List<Item> inventory) {
+    this.name = name;
+    this.currentHp = currentHp;
+    this.hpMax = hpMax;
+    this.currentMp = currentMp;
+    this.mpMax = mpMax;
+    this.inventory = inventory;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public int getCurrentHp() {
+    return currentHp;
+  }
+
+  public void setCurrentHp(int currentHp) {
+    this.currentHp = currentHp;
+  }
+
+  public int getHpMax() {
+    return hpMax;
+  }
+
+  public void setHpMax(int hpMax) {
+    this.hpMax = hpMax;
+  }
+
+  public int getCurrentMp() {
+    return currentMp;
+  }
+
+  public void setCurrentMp(int currentMp) {
+    this.currentMp = currentMp;
+  }
+
+  public int getMpMax() {
+    return mpMax;
+  }
+
+  public void setMpMax(int mpMax) {
+    this.mpMax = mpMax;
+  }
+
+  public List<Item> getInventory() {
+    return inventory;
+  }
+
+  public void setInventory(List<Item> inventory) {
+    this.inventory = inventory;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof People)) return false;
+    return id != null && id.equals(((People) o).getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return 31;
+  }
+
+  @Override
+  public String toString() {
+    return "People{"
+        + "id="
+        + id
+        + ", name='"
+        + name
+        + '\''
+        + ", currentHp="
+        + currentHp
+        + ", hpMax="
+        + hpMax
+        + ", currentMp="
+        + currentMp
+        + ", mpMax="
+        + mpMax
+        + ", inventory="
+        + inventory
+        + '}';
+  }
 }
