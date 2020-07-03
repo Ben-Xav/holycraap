f3e9f007b256cf3af3c1bf6af01b00cd59acce76
Ben Morant <52816705+benmorant@users.noreply.github.com>
2020-07-03 12:23:28 +0200

    added @Repository to daos

--- src/main/java/com/benmorant/game/holycraap/model/dao/ItemDao.java
+++ src/main/java/com/benmorant/game/holycraap/model/dao/ItemDao.java
@@ -3,7 +3,9 @@ package com.benmorant.game.holycraap.model.dao;
 import com.benmorant.game.holycraap.model.entity.Item;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemDao extends JpaRepository<Item, Integer> {
 
   Optional<Item> findByName(String name);
--- src/main/java/com/benmorant/game/holycraap/model/dao/PeopleDao.java
+++ src/main/java/com/benmorant/game/holycraap/model/dao/PeopleDao.java
@@ -3,7 +3,9 @@ package com.benmorant.game.holycraap.model.dao;
 import com.benmorant.game.holycraap.model.entity.People;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeopleDao extends JpaRepository<People, Integer> {
   Optional<People> findByName(String name);
 }
