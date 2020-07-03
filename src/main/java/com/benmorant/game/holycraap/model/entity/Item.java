a7ecdac134a8a771622dfe550edb6328b3f0e25a
Ben Morant <52816705+benmorant@users.noreply.github.com>
2020-07-03 12:04:54 +0200

    Changed generatedValue strategy from IDENTITY to SEQUENCE https://vladmihalcea.com/jpa-entity-identifier-sequence/

--- src/main/java/com/benmorant/game/holycraap/model/entity/Item.java
+++ src/main/java/com/benmorant/game/holycraap/model/entity/Item.java
@@ -18,7 +18,7 @@ public class Item implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Integer id;
 
   private String name;
--- src/main/java/com/benmorant/game/holycraap/model/entity/People.java
+++ src/main/java/com/benmorant/game/holycraap/model/entity/People.java
@@ -24,7 +24,7 @@ public class People implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Integer id;
 
   @NonNull private String name;
